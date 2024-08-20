78841b1ffa2b6256d83831c3ccdb712e540e44c6
delta <delta@deltalab.dev>
2024-08-20 09:48:18 +0900

    ✨feat: zerolog logging ♻️ refactor: structure

--- go.mod
+++ go.mod
@@ -3,21 +3,25 @@ module update-manifest
 go 1.22
 
 require (
-	github.com/minio/minio-go/v7 v7.0.71
-	golang.org/x/crypto v0.24.0
+	github.com/minio/minio-go/v7 v7.0.75
+	github.com/rs/zerolog v1.33.0
+	golang.org/x/crypto v0.26.0
 )
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/xid v1.5.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -12,162 +12,24 @@ import (
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/blake2b"
 )
 
-func main() {
-	AccountID, exists := os.LookupEnv("ACCOUNT_ID")
-	if !exists {
-		fmt.Println("E: ACCOUNT_ID is not set")
-		os.Exit(1)
-	}
-
-	AccessKey, exists := os.LookupEnv("ACCESS_KEY")
-	if !exists {
-		fmt.Println("E: ACCESS_KEY is not set")
-		os.Exit(1)
-	}
-
-	AccessSecret, exists := os.LookupEnv("ACCESS_SECRET")
-	if !exists {
-		fmt.Println("E: ACCESS_SECRET is not set")
-		os.Exit(1)
-	}
-
-	Bucket, exists := os.LookupEnv("BUCKET")
-	if !exists {
-		fmt.Println("E: BUCKET is not set")
-		os.Exit(1)
-	}
-
-	ReleaseChannel, exists := os.LookupEnv("CHANNEL")
-	if !exists {
-		fmt.Println("E: CHANNEL is not set")
-		os.Exit(1)
-	}
-
-	AppID, exists := os.LookupEnv("APP_ID")
-	if !exists {
-		fmt.Println("E: APP_ID is not set")
-		os.Exit(1)
-	}
-
-	Version, exists := os.LookupEnv("VERSION")
-	if !exists {
-		fmt.Println("E: VERSION is not set")
-		os.Exit(1)
-	}
-
-	Platform, exists := os.LookupEnv("PLATFORM")
-	if !exists {
-		fmt.Println("E: PLATFORM is not set")
-		os.Exit(1)
-	}
-
-	ExecutablePath, exists := os.LookupEnv("EXECUTABLE_PATH")
-	if !exists {
-		fmt.Println("E: EXECUTABLE_PATH is not set")
-		os.Exit(1)
-	}
-
-	executable, err := os.Open(ExecutablePath)
-	if err != nil {
-		fmt.Printf("E: Failed to open executable: %v\n", err)
-		os.Exit(1)
-	}
-
-	executableStat, err := executable.Stat()
-	if err != nil {
-		fmt.Printf("E: Failed to stat executable: %v\n", err)
-		os.Exit(1)
-	}
-
-	r2, err := minio.NewCore(fmt.Sprintf("%s.r2.cloudflarestorage.com", AccountID), &minio.Options{
-		Secure: true,
-		Creds:  credentials.NewStaticV4(AccessKey, AccessSecret, ""),
-		Region: "auto",
-	})
-
-	if err != nil {
-		fmt.Printf("E: Failed to connect to r2: %v\n", err)
-		os.Exit(1)
-	}
-
-	var manifest Manifest
-
-	// lookup if manifest exists
-	reader, _, _, err := r2.GetObject(context.Background(), Bucket, fmt.Sprintf("%s/manifest.json", AppID), minio.GetObjectOptions{})
-	if err == nil {
-		if err := json.NewDecoder(reader).Decode(&manifest); err != nil {
-			fmt.Printf("E: Failed to decode manifest: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	if manifest.Channel == nil {
-		manifest.Channel = make(map[string]*Channel)
-	}
-
-	if _, ok := manifest.Channel[ReleaseChannel]; !ok {
-		manifest.Channel[ReleaseChannel] = &Channel{
-			Artifact: make(map[string]*Artifact),
-		}
-	}
-
-	if _, ok := manifest.Channel[ReleaseChannel].Artifact[Platform]; !ok {
-		manifest.Channel[ReleaseChannel].Artifact[Platform] = &Artifact{}
-	}
-
-	manifest.Channel[ReleaseChannel].Version = Version
-
-	// create blake2b checksum
-	hasher, _ := blake2b.New256(nil)
-	if _, err := io.Copy(hasher, executable); err != nil {
-		fmt.Printf("E: Failed to create checksum: %v\n", err)
-		os.Exit(1)
-	}
-
-	manifest.Channel[ReleaseChannel].Artifact[Platform].Checksum = hex.EncodeToString(hasher.Sum(nil))
-
-	_, err = executable.Seek(0, 0)
-	if err != nil {
-		fmt.Printf("E: Failed to seek to beginning of executable: %v\n", err)
-		os.Exit(1)
-	}
-
-	_, err = r2.Client.PutObject(context.Background(), Bucket, fmt.Sprintf("%s/artifect/%s", AppID, manifest.Channel[ReleaseChannel].Artifact[Platform].Checksum), executable, executableStat.Size(), minio.PutObjectOptions{
-		ContentType: "application/octet-stream",
-	})
-	if err != nil {
-		fmt.Printf("E: Failed to upload artifact: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("I: Artifact uploaded successfully")
-
-	manifest.Channel[ReleaseChannel].Artifact[Platform].Binary = fmt.Sprintf("%s/artifect/%s", AppID, manifest.Channel[ReleaseChannel].Artifact[Platform].Checksum)
-	manifest.Channel[ReleaseChannel].Build = executableStat.ModTime()
-
-	marshaledManifest, err := json.Marshal(manifest)
-	if err != nil {
-		fmt.Printf("E: Failed to marshal manifest: %v\n", err)
-		os.Exit(1)
-	}
-
-	_, err = r2.Client.PutObject(context.Background(), Bucket, fmt.Sprintf("%s/manifest.json", AppID), bytes.NewReader(marshaledManifest), int64(len(marshaledManifest)), minio.PutObjectOptions{
-		ContentType: "application/json",
-	})
-
-	if err != nil {
-		fmt.Printf("E: Failed to upload manifest: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("I: Manifest uploaded successfully")
+type Config struct {
+	AccountID      string
+	AccessKey      string
+	AccessSecret   string
+	Bucket         string
+	Channel        string
+	AppID          string
+	Version        string
+	Platform       string
+	ExecutablePath string
 }
 
 type Manifest struct {
-	// Channel can be "stable" or "beta"
 	Channel map[string]*Channel `json:"channel"`
 }
 
@@ -183,4 +45,172 @@ type Artifact struct {
 	Checksum string         `json:"checksum"`
 	Patch    string         `json:"patch"`
 	Metadata map[string]any `json:"metadata"`
+}
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
+
+func loadConfig() (*Config, error) {
+	config := &Config{}
+	envVars := []struct {
+		ptr *string
+		key string
+	}{
+		{&config.AccountID, "ACCOUNT_ID"},
+		{&config.AccessKey, "ACCESS_KEY"},
+		{&config.AccessSecret, "ACCESS_SECRET"},
+		{&config.Bucket, "BUCKET"},
+		{&config.Channel, "CHANNEL"},
+		{&config.AppID, "APP_ID"},
+		{&config.Version, "VERSION"},
+		{&config.Platform, "PLATFORM"},
+		{&config.ExecutablePath, "EXECUTABLE_PATH"},
+	}
+
+	for _, env := range envVars {
+		value, exists := os.LookupEnv(env.key)
+		if !exists {
+			return nil, fmt.Errorf("%s is not set", env.key)
+		}
+		*env.ptr = value
+	}
+
+	return config, nil
+}
+
+func createR2Client(config *Config) (*minio.Core, error) {
+	return minio.NewCore(
+		fmt.Sprintf("%s.r2.cloudflarestorage.com", config.AccountID),
+		&minio.Options{
+			Secure: true,
+			Creds:  credentials.NewStaticV4(config.AccessKey, config.AccessSecret, ""),
+			Region: "auto",
+		},
+	)
+}
+
+func getManifest(ctx context.Context, r2 *minio.Core, bucket, appID string) (*Manifest, error) {
+	reader, _, _, err := r2.GetObject(ctx, bucket, fmt.Sprintf("%s/manifest.json", appID), minio.GetObjectOptions{})
+	if err != nil {
+		if minio.ToErrorResponse(err).Code == "NoSuchKey" {
+			return &Manifest{Channel: make(map[string]*Channel)}, nil
+		}
+		return nil, fmt.Errorf("failed to get manifest: %w", err)
+	}
+	defer func(reader io.ReadCloser) {
+		err := reader.Close()
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to close reader")
+		}
+	}(reader)
+
+	var manifest Manifest
+	if err := json.NewDecoder(reader).Decode(&manifest); err != nil {
+		return nil, fmt.Errorf("failed to decode manifest: %w", err)
+	}
+	return &manifest, nil
+}
+
+func calculateChecksum(file *os.File) (string, error) {
+	hasher, _ := blake2b.New256(nil)
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", fmt.Errorf("failed to create checksum: %w", err)
+	}
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func uploadArtifact(ctx context.Context, r2 *minio.Core, config *Config, file *os.File, fileSize int64, checksum string) error {
+	_, err := r2.Client.PutObject(ctx, config.Bucket, fmt.Sprintf("%s/artifact/%s", config.AppID, checksum), file, fileSize, minio.PutObjectOptions{
+		ContentType: "application/octet-stream",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload artifact: %w", err)
+	}
+	return nil
+}
+
+func uploadManifest(ctx context.Context, r2 *minio.Core, config *Config, manifest *Manifest) error {
+	marshaledManifest, err := json.Marshal(manifest)
+	if err != nil {
+		return fmt.Errorf("failed to marshal manifest: %w", err)
+	}
+
+	_, err = r2.Client.PutObject(ctx, config.Bucket, fmt.Sprintf("%s/manifest.json", config.AppID), bytes.NewReader(marshaledManifest), int64(len(marshaledManifest)), minio.PutObjectOptions{
+		ContentType: "application/json",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload manifest: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load configuration")
+	}
+
+	r2, err := createR2Client(config)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to R2")
+	}
+
+	ctx := context.Background()
+
+	manifest, err := getManifest(ctx, r2, config.Bucket, config.AppID)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get manifest")
+	}
+
+	executable, err := os.Open(config.ExecutablePath)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to open executable")
+	}
+	defer func(executable *os.File) {
+		err := executable.Close()
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to close executable")
+		}
+	}(executable)
+
+	executableStat, err := executable.Stat()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to stat executable")
+	}
+
+	checksum, err := calculateChecksum(executable)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to calculate checksum")
+	}
+
+	if _, err := executable.Seek(0, 0); err != nil {
+		log.Fatal().Err(err).Msg("Failed to seek to beginning of executable")
+	}
+
+	if err := uploadArtifact(ctx, r2, config, executable, executableStat.Size(), checksum); err != nil {
+		log.Fatal().Err(err).Msg("Failed to upload artifact")
+	}
+
+	log.Info().Msg("Artifact uploaded successfully")
+
+	if manifest.Channel[config.Channel] == nil {
+		manifest.Channel[config.Channel] = &Channel{
+			Artifact: make(map[string]*Artifact),
+		}
+	}
+
+	manifest.Channel[config.Channel].Version = config.Version
+	manifest.Channel[config.Channel].Build = executableStat.ModTime()
+	manifest.Channel[config.Channel].Artifact[config.Platform] = &Artifact{
+		Binary:   fmt.Sprintf("%s/artifact/%s", config.AppID, checksum),
+		Checksum: checksum,
+	}
+
+	if err := uploadManifest(ctx, r2, config, manifest); err != nil {
+		log.Fatal().Err(err).Msg("Failed to upload manifest")
+	}
+
+	log.Info().Msg("Manifest uploaded successfully")
 }
